886c8ce8a100a52f3246914b6e3891616c81aa38
Matt Mazzola <mattm@microsoft.com>
2016-08-15 17:13:17 -0700

    Update gulp build to copy css files, and concat vendor files to dist folder so it is can be deployed without dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp-help')(require('gulp')),
     $ = require('gulp-load-plugins')({ lazy: true }),
     config = require('./gulp/config')(),
+    del = require('del'),
     merge2 = require('merge2'),
     moment = require('moment'),
     runSequence = require('run-sequence'),
@@ -10,7 +11,9 @@ var gulp = require('gulp-help')(require('gulp')),
 
 gulp.task('build', function (done) {
     runSequence(
+        'clean:dist',
         'compile:src',
+        ['copycss', 'vendor'],
         ['templates', 'replace'],
         done
     );
@@ -26,6 +29,11 @@ gulp.task('ghpages', 'Deploy application to gh-pages branch', function () {
 
 gulp.task('compile:watch', 'Watch sources', function () {
     gulp.watch(['./app/**/*.ts', './app/**/*.html'], ['compile:src', 'templates', 'replace']);
+});
+
+gulp.task('clean:dist', function() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['dist']);
 });
 
 gulp.task('compile:src', 'Compile typescript for library', function() {
@@ -59,10 +67,32 @@ gulp.task('templates', function () {
         ;
 });
 
+gulp.task('copycss', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './app/styles/app.css'
+    ])
+        .pipe(gulp.dest(config.distFolder))
+        ;
+});
+
+gulp.task('vendor', function () {
+    return gulp.src([
+        './node_modules/angular/angular.js',
+        './node_modules/angular-ui-router/release/angular-ui-router.js',
+        './node_modules/powerbi-client/dist/powerbi.js',
+        './node_modules/angular-powerbi/dist/angular-powerbi.js'
+    ])
+        .pipe($.concat('vendor.js'))
+        .pipe(gulp.dest(config.distFolder))
+    ;
+});
+
 gulp.task('replace', function () {
     return gulp.src(config.htmlPage)
         .pipe($.htmlReplace({
-            'js': ['app.js', 'app.templates.js']
+            'css': ['bootstrap.css', 'app.css'],
+            'js': ['vendor.js', 'app.js', 'app.templates.js']
         }))
         .pipe(gulp.dest(config.distFolder))
         ;
